5616b93c1e643c84e4399241687d9e6559568594
woclass <git@wo-class.cn>
2022-01-17 01:13:49 +0800

    [test] add "hello-window"

---
+++ examples/hello-window/main.c
@@ -0,0 +1,39 @@
+#include <stdlib.h> // exit
+#include <string.h> // memset
+#include <stdio.h> // fprintf
+#include "../../ui.h"
+
+
+int onClosing(uiWindow *w, void *data)
+{
+    uiQuit();
+    return 1;
+}
+
+int main(void)
+{
+    uiInitOptions o;
+    const char *err;
+    uiWindow *w;
+    uiLabel *label;
+
+    memset(&o, 0, sizeof (uiInitOptions));
+    err = uiInit(&o);
+    if (err != NULL) {
+        fprintf(stderr, "error initializing ui: %s\n", err);
+        uiFreeInitError(err);
+        return 1;
+    }
+
+    // Create a new window
+    w = uiNewWindow("Hello Window", 300, 30, 0);
+    uiWindowOnClosing(w, onClosing, NULL);
+    
+    label = uiNewLabel("Hello, World!");
+    uiWindowSetChild(w, uiControl(label));
+
+    uiControlShow(uiControl(w));
+    uiMain();
+    uiUninit();
+    return 0;
+}
