9ea000c257ec01ede2a5c145f5f2968274935bc0
Angelo Haller <angelo@szanni.org>
2022-05-26 09:33:15 -0500

    Document uiImage using Doxygen formatting.

--- ui.h
+++ ui.h
@@ -2577,47 +2577,65 @@ _UI_EXTERN int uiGridPadded(uiGrid *g);
 _UI_EXTERN void uiGridSetPadded(uiGrid *g, int padded);
 _UI_EXTERN uiGrid *uiNewGrid(void);
 
-// uiImage stores an image for display on screen.
-// 
-// Images are built from one or more representations, each with the
-// same aspect ratio but a different pixel size. libui automatically
-// selects the most appropriate representation for drawing the image
-// when it comes time to draw the image; what this means depends
-// on the pixel density of the target context. Therefore, one can use
-// uiImage to draw higher-detailed images on higher-density
-// displays. The typical use cases are either:
-// 
-// 	- have just a single representation, at which point all screens
-// 	  use the same image, and thus uiImage acts like a simple
-// 	  bitmap image, or
-// 	- have two images, one at normal resolution and one at 2x
-// 	  resolution; this matches the current expectations of some
-// 	  desktop systems at the time of writing (mid-2018)
-// 
-// uiImage is very simple: it only supports premultiplied 32-bit
-// RGBA images, and libui does not provide any image file loading
-// or image format conversion utilities on top of that.
+/**
+ * A container for an image to be displayed on screen.
+ *
+ * The container can hold multiple representations of the same image with the
+ * _same_ aspect ratio but in different resolutions to support high-density
+ * displays.
+ *
+ * Common image dimension scale factors are `1x` and `2x`. Providing higher
+ * density representations is entirely optional.
+ *
+ * The system will automatically determine the correct image to render depending
+ * on the screen's pixel density.
+ *
+ * uiImage only supports premultiplied 32-bit RGBA images.
+ *
+ * No image file loading or image format conversion utilities are provided.
+ *
+ * @struct uiImage
+ * @ingroup static
+ */
 typedef struct uiImage uiImage;
 
-// @role uiImage constructor
-// uiNewImage creates a new uiImage with the given width and
-// height. This width and height should be the size in points of the
-// image in the device-independent case; typically this is the 1x size.
-// TODO for all uiImage functions: use const void * for const correctness
+/**
+ * Creates a new image container.
+ *
+ * Dimensions are measured in points. This is most commonly the pixel size
+ * of the `1x` scaled image.
+ *
+ * @param width Width in points.
+ * @param height Height in points.
+ * @returns A new uiImage instance.
+ * @memberof uiImage
+ */
 _UI_EXTERN uiImage *uiNewImage(double width, double height);
 
-// @role uiImage destructor
-// uiFreeImage frees the given image and all associated resources.
+/**
+ * Frees the image container and all associated resources.
+ *
+ * @param i uiImage instance.
+ * @memberof uiImage
+ */
 _UI_EXTERN void uiFreeImage(uiImage *i);
 
-// uiImageAppend adds a representation to the uiImage.
-// pixels should point to a byte array of premultiplied pixels
-// stored in [R G B A] order (so ((uint8_t *) pixels)[0] is the R of the
-// first pixel and [3] is the A of the first pixel). pixelWidth and
-// pixelHeight is the size *in pixels* of the image, and pixelStride is
-// the number *of bytes* per row of the pixels array. Therefore,
-// pixels itself must be at least byteStride * pixelHeight bytes long.
-// TODO see if we either need the stride or can provide a way to get the OS-preferred stride (in cairo we do)
+/**
+ * Appends a new image representation.
+ *
+ * @param i uiImage instance.
+ * @param pixels Byte array of premultiplied pixels in [R G B A] order.\n
+ *               `((uint8_t *) pixels)[0]` equals the **R** of the first pixel,
+ *               `[3]` the **A** of the first pixel.\n
+ *               `pixels` must be at least `byteStride * pixelHeight` bytes long.\n
+ *               Data is owned by the caller.
+ * @param pixelWidth Width in pixels.
+ * @param pixelHeight Height in pixels.
+ * @param byteStride Number of bytes per row of the pixel array.
+ * @todo see if we either need the stride or can provide a way to get the OS-preferred stride (in cairo we do)
+ * @todo use const void * for const correctness
+ * @memberof uiImage
+ */
 _UI_EXTERN void uiImageAppend(uiImage *i, void *pixels, int pixelWidth, int pixelHeight, int byteStride);
 
 // uiTableValue stores a value to be passed along uiTable and
