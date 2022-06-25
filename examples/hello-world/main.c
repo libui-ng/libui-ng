890d597834ddd693735ff6ab6c6209b7c119e09e
woclass <git@wo-class.cn>
2022-06-25 21:23:07 +0800

    [test] use tab

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -6,30 +6,30 @@
 
 int onClosing(uiWindow *w, void *data)
 {
-    uiQuit();
-    return 1;
+	uiQuit();
+	return 1;
 }
 
 int main(void)
 {
-    uiInitOptions o;
+	uiInitOptions o;
 	const char *err;
-    uiWindow *w;
+	uiWindow *w;
 
-    memset(&o, 0, sizeof (uiInitOptions));
-    err = uiInit(&o);
+	memset(&o, 0, sizeof (uiInitOptions));
+	err = uiInit(&o);
 	if (err != NULL) {
 		fprintf(stderr, "error initializing ui: %s\n", err);
 		uiFreeInitError(err);
 		return 1;
 	}
 
-    // Create a new window
-    w = uiNewWindow("Hello, World!", 300, 30, 0);
+	// Create a new window
+	w = uiNewWindow("Hello, World!", 300, 30, 0);
 
-    uiWindowOnClosing(w, onClosing, NULL);
-    uiControlShow(uiControl(w));
-    uiMain();
-    uiUninit();
-    return 0;
+	uiWindowOnClosing(w, onClosing, NULL);
+	uiControlShow(uiControl(w));
+	uiMain();
+	uiUninit();
+	return 0;
 }
