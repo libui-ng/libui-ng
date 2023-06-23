2c3cfe24b449cda84c65c08b481dfeb1b247063d
Fs <Fsu0413@vip.qq.com>
2023-06-23 09:20:03 +0800

    newer MinGW-w64 have this ABI bug workaround under macro WIDL_EXPLICIT_AGGREGATE_RETURNS (defined by default in newer MinGW-w64)

--- windows/winutil.cpp
+++ windows/winutil.cpp
@@ -140,7 +140,7 @@ void invalidateRect(HWND hwnd, RECT *r, BOOL erase)
 // that damn ABI bug is never going to escape me is it
 D2D1_SIZE_F realGetSize(ID2D1RenderTarget *rt)
 {
-#ifdef _MSC_VER
+#if defined(_MSC_VER) || defined(WIDL_EXPLICIT_AGGREGATE_RETURNS)
 	return rt->GetSize();
 #else
 	D2D1_SIZE_F size;
