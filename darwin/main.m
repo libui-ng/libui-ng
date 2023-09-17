d539866368bf41f43a65460c248da751c7863f11
matyalatte <matyalatte@gmail.com>
2023-09-17 15:56:40 +0900

    use a proper way to activate based on the bundle info

--- darwin/main.m
+++ darwin/main.m
@@ -108,12 +108,31 @@ static BOOL stepsIsRunning;
 
 -(void)applicationDidFinishLaunching:(NSNotification *)notification
 {
-	// don't use setActivationPolicy outside this function
-	// or the menu bar won't work properly on macOS 10.15
-	// see https://github.com/libui-ng/libui-ng/issues/225
-	// don't check for a NO return; something (launch services?) causes running from application bundles to always return NO when asking to change activation policy, even if the change is to the same activation policy!
-	// see https://github.com/andlabs/ui/issues/6
-	[NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+	BOOL activate = NO;
+	CFURLRef url = CFBundleCopyBundleURL(CFBundleGetMainBundle());
+	CFStringRef path = CFURLCopyFileSystemPath (url, kCFURLPOSIXPathStyle);
+	CFRelease(url);
+
+	if (!CFStringHasSuffix(path, @".app")) {
+		// don't use setActivationPolicy outside this function
+		// or the menu bar won't work properly on macOS 10.15
+		// see https://github.com/libui-ng/libui-ng/issues/225
+
+		// don't check for a NO return;
+		// something (launch services?) causes running from application bundles to always return NO
+		// when asking to change activation policy, even if the change is to the same activation policy!
+		// see https://github.com/andlabs/ui/issues/6
+		[uiprivNSApp() setActivationPolicy:NSApplicationActivationPolicyRegular];
+		activate = YES;
+	} else if ([uiprivNSApp() activationPolicy] == NSApplicationActivationPolicyAccessory) {
+		// The app is an agent app that runs in the background and doesn't appear in the Dock.
+		activate = YES;
+	}
+
+	CFRelease(path);
+
+	if (activate)
+		[uiprivNSApp() activateIgnoringOtherApps: YES];
 }
 
 @end
