dfb5c4b595a52bc981e0028500e327b5e3f619d7
desttinghim <contact@louispearson.work>
2023-10-12 16:40:56 -0600

    Make resource compiler arg work with resinator

--- build.zig
+++ build.zig
@@ -30,22 +30,25 @@ pub fn build(b: *std.Build) void {
     lib.addIncludePath(.{ .path = "common" });
     lib.installHeader("ui.h", "ui.h");
     lib.defineCMacro("libui_EXPORTS", "");
-    lib.addCSourceFiles(&.{
-        "common/areaevents.c",
-        "common/attribute.c",
-        "common/attrlist.c",
-        "common/attrstr.c",
-        "common/control.c",
-        "common/debug.c",
-        "common/matrix.c",
-        "common/opentype.c",
-        "common/shouldquit.c",
-        "common/table.c",
-        "common/tablemodel.c",
-        "common/tablevalue.c",
-        "common/userbugs.c",
-        "common/utf.c",
-    }, &.{});
+    lib.addCSourceFiles(.{
+        .files = &.{
+            "common/areaevents.c",
+            "common/attribute.c",
+            "common/attrlist.c",
+            "common/attrstr.c",
+            "common/control.c",
+            "common/debug.c",
+            "common/matrix.c",
+            "common/opentype.c",
+            "common/shouldquit.c",
+            "common/table.c",
+            "common/tablemodel.c",
+            "common/tablevalue.c",
+            "common/userbugs.c",
+            "common/utf.c",
+        },
+        .flags = &.{},
+    });
 
     if (target.isDarwin()) {
         // use darwin/*.m backend
@@ -54,58 +57,61 @@ pub fn build(b: *std.Build) void {
         lib.linkFramework("Foundation");
         lib.linkFramework("Appkit");
         lib.addSystemIncludePath(.{ .path = "Cocoa" });
-        lib.addCSourceFiles(&.{
-            "darwin/aat.m",
-            "darwin/alloc.m",
-            "darwin/areaevents.m",
-            "darwin/area.m",
-            "darwin/attrstr.m",
-            "darwin/autolayout.m",
-            "darwin/box.m",
-            "darwin/button.m",
-            "darwin/checkbox.m",
-            "darwin/colorbutton.m",
-            "darwin/combobox.m",
-            "darwin/control.m",
-            "darwin/datetimepicker.m",
-            "darwin/debug.m",
-            "darwin/draw.m",
-            "darwin/drawtext.m",
-            "darwin/editablecombo.m",
-            "darwin/entry.m",
-            "darwin/event.m",
-            "darwin/fontbutton.m",
-            "darwin/fontmatch.m",
-            "darwin/fonttraits.m",
-            "darwin/fontvariation.m",
-            "darwin/form.m",
-            "darwin/future.m",
-            "darwin/graphemes.m",
-            "darwin/grid.m",
-            "darwin/group.m",
-            "darwin/image.m",
-            "darwin/label.m",
-            "darwin/main.m",
-            "darwin/menu.m",
-            "darwin/multilineentry.m",
-            "darwin/nstextfield.m",
-            "darwin/opentype.m",
-            "darwin/progressbar.m",
-            "darwin/radiobuttons.m",
-            "darwin/scrollview.m",
-            "darwin/separator.m",
-            "darwin/slider.m",
-            "darwin/spinbox.m",
-            "darwin/stddialogs.m",
-            "darwin/tablecolumn.m",
-            "darwin/table.m",
-            "darwin/tab.m",
-            "darwin/text.m",
-            "darwin/undocumented.m",
-            "darwin/util.m",
-            "darwin/window.m",
-            "darwin/winmoveresize.m",
-        }, &.{});
+        lib.addCSourceFiles(.{
+            .files = &.{
+                "darwin/aat.m",
+                "darwin/alloc.m",
+                "darwin/areaevents.m",
+                "darwin/area.m",
+                "darwin/attrstr.m",
+                "darwin/autolayout.m",
+                "darwin/box.m",
+                "darwin/button.m",
+                "darwin/checkbox.m",
+                "darwin/colorbutton.m",
+                "darwin/combobox.m",
+                "darwin/control.m",
+                "darwin/datetimepicker.m",
+                "darwin/debug.m",
+                "darwin/draw.m",
+                "darwin/drawtext.m",
+                "darwin/editablecombo.m",
+                "darwin/entry.m",
+                "darwin/event.m",
+                "darwin/fontbutton.m",
+                "darwin/fontmatch.m",
+                "darwin/fonttraits.m",
+                "darwin/fontvariation.m",
+                "darwin/form.m",
+                "darwin/future.m",
+                "darwin/graphemes.m",
+                "darwin/grid.m",
+                "darwin/group.m",
+                "darwin/image.m",
+                "darwin/label.m",
+                "darwin/main.m",
+                "darwin/menu.m",
+                "darwin/multilineentry.m",
+                "darwin/nstextfield.m",
+                "darwin/opentype.m",
+                "darwin/progressbar.m",
+                "darwin/radiobuttons.m",
+                "darwin/scrollview.m",
+                "darwin/separator.m",
+                "darwin/slider.m",
+                "darwin/spinbox.m",
+                "darwin/stddialogs.m",
+                "darwin/tablecolumn.m",
+                "darwin/table.m",
+                "darwin/tab.m",
+                "darwin/text.m",
+                "darwin/undocumented.m",
+                "darwin/util.m",
+                "darwin/window.m",
+                "darwin/winmoveresize.m",
+            },
+            .flags = &.{},
+        });
     } else if (target.isWindows()) {
         // use windows/*.cpp backend
         lib.installHeader("ui_windows.h", "ui_windows.h");
@@ -128,126 +134,140 @@ pub fn build(b: *std.Build) void {
         lib.linkLibCpp();
 
         // Compile
-        if (lib.isDynamicLibrary()) {
-            const rc_command = b.addSystemCommand(&.{ resource_compiler, "windows/resources.rc" });
-            const output = rc_command.addPrefixedOutputFileArg("-fo", "resources.res");
+        if (is_dynamic) {
+            const rc_command = b.addSystemCommand(&.{resource_compiler});
+            // options
+            rc_command.addArgs(&.{ "/:auto-includes", "gnu" });
+
+            // input file
+            rc_command.addArg("windows/resources.rc");
+            // output file
+            const output = rc_command.addOutputFileArg("resources.res");
+
             lib.addObjectFile(output);
+            lib.step.dependOn(&rc_command.step);
         }
 
-        lib.addCSourceFiles(&.{
-            "windows/alloc.cpp",
-            "windows/area.cpp",
-            "windows/areadraw.cpp",
-            "windows/areaevents.cpp",
-            "windows/areascroll.cpp",
-            "windows/areautil.cpp",
-            "windows/attrstr.cpp",
-            "windows/box.cpp",
-            "windows/button.cpp",
-            "windows/checkbox.cpp",
-            "windows/colorbutton.cpp",
-            "windows/colordialog.cpp",
-            "windows/combobox.cpp",
-            "windows/container.cpp",
-            "windows/control.cpp",
-            "windows/d2dscratch.cpp",
-            "windows/datetimepicker.cpp",
-            "windows/debug.cpp",
-            "windows/draw.cpp",
-            "windows/drawmatrix.cpp",
-            "windows/drawpath.cpp",
-            "windows/drawtext.cpp",
-            "windows/dwrite.cpp",
-            "windows/editablecombo.cpp",
-            "windows/entry.cpp",
-            "windows/events.cpp",
-            "windows/fontbutton.cpp",
-            "windows/fontdialog.cpp",
-            "windows/fontmatch.cpp",
-            "windows/form.cpp",
-            "windows/graphemes.cpp",
-            "windows/grid.cpp",
-            "windows/group.cpp",
-            "windows/image.cpp",
-            "windows/init.cpp",
-            "windows/label.cpp",
-            "windows/main.cpp",
-            "windows/menu.cpp",
-            "windows/multilineentry.cpp",
-            "windows/opentype.cpp",
-            "windows/parent.cpp",
-            "windows/progressbar.cpp",
-            "windows/radiobuttons.cpp",
-            "windows/separator.cpp",
-            "windows/sizing.cpp",
-            "windows/slider.cpp",
-            "windows/spinbox.cpp",
-            "windows/stddialogs.cpp",
-            "windows/tab.cpp",
-            "windows/table.cpp",
-            "windows/tabledispinfo.cpp",
-            "windows/tabledraw.cpp",
-            "windows/tableediting.cpp",
-            "windows/tablemetrics.cpp",
-            "windows/tabpage.cpp",
-            "windows/text.cpp",
-            "windows/utf16.cpp",
-            "windows/utilwin.cpp",
-            "windows/window.cpp",
-            "windows/winpublic.cpp",
-            "windows/winutil.cpp",
-        }, &.{});
+        lib.addCSourceFiles(.{
+            .files = &.{
+                "windows/alloc.cpp",
+                "windows/area.cpp",
+                "windows/areadraw.cpp",
+                "windows/areaevents.cpp",
+                "windows/areascroll.cpp",
+                "windows/areautil.cpp",
+                "windows/attrstr.cpp",
+                "windows/box.cpp",
+                "windows/button.cpp",
+                "windows/checkbox.cpp",
+                "windows/colorbutton.cpp",
+                "windows/colordialog.cpp",
+                "windows/combobox.cpp",
+                "windows/container.cpp",
+                "windows/control.cpp",
+                "windows/d2dscratch.cpp",
+                "windows/datetimepicker.cpp",
+                "windows/debug.cpp",
+                "windows/draw.cpp",
+                "windows/drawmatrix.cpp",
+                "windows/drawpath.cpp",
+                "windows/drawtext.cpp",
+                "windows/dwrite.cpp",
+                "windows/editablecombo.cpp",
+                "windows/entry.cpp",
+                "windows/events.cpp",
+                "windows/fontbutton.cpp",
+                "windows/fontdialog.cpp",
+                "windows/fontmatch.cpp",
+                "windows/form.cpp",
+                "windows/graphemes.cpp",
+                "windows/grid.cpp",
+                "windows/group.cpp",
+                "windows/image.cpp",
+                "windows/init.cpp",
+                "windows/label.cpp",
+                "windows/main.cpp",
+                "windows/menu.cpp",
+                "windows/multilineentry.cpp",
+                "windows/opentype.cpp",
+                "windows/parent.cpp",
+                "windows/progressbar.cpp",
+                "windows/radiobuttons.cpp",
+                "windows/separator.cpp",
+                "windows/sizing.cpp",
+                "windows/slider.cpp",
+                "windows/spinbox.cpp",
+                "windows/stddialogs.cpp",
+                "windows/tab.cpp",
+                "windows/table.cpp",
+                "windows/tabledispinfo.cpp",
+                "windows/tabledraw.cpp",
+                "windows/tableediting.cpp",
+                "windows/tablemetrics.cpp",
+                "windows/tabpage.cpp",
+                "windows/text.cpp",
+                "windows/utf16.cpp",
+                "windows/utilwin.cpp",
+                "windows/window.cpp",
+                "windows/winpublic.cpp",
+                "windows/winutil.cpp",
+            },
+            .flags = &.{},
+        });
     } else {
         // assume unix/*.c backend
         lib.installHeader("ui_unix.h", "ui_unix.h");
         lib.linkSystemLibrary("gtk+-3.0");
         lib.addIncludePath(.{ .path = "unix" });
-        lib.addCSourceFiles(&.{
-            "unix/alloc.c",
-            "unix/area.c",
-            "unix/attrstr.c",
-            "unix/box.c",
-            "unix/button.c",
-            "unix/cellrendererbutton.c",
-            "unix/checkbox.c",
-            "unix/child.c",
-            "unix/colorbutton.c",
-            "unix/combobox.c",
-            "unix/control.c",
-            "unix/datetimepicker.c",
-            "unix/debug.c",
-            "unix/draw.c",
-            "unix/drawmatrix.c",
-            "unix/drawpath.c",
-            "unix/drawtext.c",
-            "unix/editablecombo.c",
-            "unix/entry.c",
-            "unix/fontbutton.c",
-            "unix/fontmatch.c",
-            "unix/form.c",
-            "unix/future.c",
-            "unix/graphemes.c",
-            "unix/grid.c",
-            "unix/group.c",
-            "unix/image.c",
-            "unix/label.c",
-            "unix/main.c",
-            "unix/menu.c",
-            "unix/multilineentry.c",
-            "unix/opentype.c",
-            "unix/progressbar.c",
-            "unix/radiobuttons.c",
-            "unix/separator.c",
-            "unix/slider.c",
-            "unix/spinbox.c",
-            "unix/stddialogs.c",
-            "unix/tab.c",
-            "unix/table.c",
-            "unix/tablemodel.c",
-            "unix/text.c",
-            "unix/util.c",
-            "unix/window.c",
-        }, &.{});
+        lib.addCSourceFiles(.{
+            .files = &.{
+                "unix/alloc.c",
+                "unix/area.c",
+                "unix/attrstr.c",
+                "unix/box.c",
+                "unix/button.c",
+                "unix/cellrendererbutton.c",
+                "unix/checkbox.c",
+                "unix/child.c",
+                "unix/colorbutton.c",
+                "unix/combobox.c",
+                "unix/control.c",
+                "unix/datetimepicker.c",
+                "unix/debug.c",
+                "unix/draw.c",
+                "unix/drawmatrix.c",
+                "unix/drawpath.c",
+                "unix/drawtext.c",
+                "unix/editablecombo.c",
+                "unix/entry.c",
+                "unix/fontbutton.c",
+                "unix/fontmatch.c",
+                "unix/form.c",
+                "unix/future.c",
+                "unix/graphemes.c",
+                "unix/grid.c",
+                "unix/group.c",
+                "unix/image.c",
+                "unix/label.c",
+                "unix/main.c",
+                "unix/menu.c",
+                "unix/multilineentry.c",
+                "unix/opentype.c",
+                "unix/progressbar.c",
+                "unix/radiobuttons.c",
+                "unix/separator.c",
+                "unix/slider.c",
+                "unix/spinbox.c",
+                "unix/stddialogs.c",
+                "unix/tab.c",
+                "unix/table.c",
+                "unix/tablemodel.c",
+                "unix/text.c",
+                "unix/util.c",
+                "unix/window.c",
+            },
+            .flags = &.{},
+        });
     }
 
     b.installArtifact(lib);
@@ -280,10 +300,18 @@ pub fn build(b: *std.Build) void {
         });
         exe.linkLibrary(lib);
         if (target.isWindows()) {
-            const rc_command = b.addSystemCommand(&.{ resource_compiler, "examples/resources.rc" });
-            const output = rc_command.addPrefixedOutputFileArg("-fo", "resources.res");
-            if (!is_dynamic) rc_command.addArg("-D_UI_STATIC");
-            lib.addObjectFile(output);
+            const rc_command = b.addSystemCommand(&.{resource_compiler});
+            // options
+            rc_command.addArgs(&.{ "/:auto-includes", "gnu" });
+            if (!is_dynamic) rc_command.addArgs(&.{ "/d", "_UI_STATIC" });
+
+            // input file
+            rc_command.addArg("examples/resources.rc");
+            // output file
+            const output = rc_command.addOutputFileArg("resources.res");
+
+            exe.addObjectFile(output);
+            exe.step.dependOn(&rc_command.step);
         }
         b.installArtifact(exe);
     }
@@ -301,10 +329,18 @@ pub fn build(b: *std.Build) void {
         exe.linkLibCpp();
 
         if (target.isWindows()) {
-            const rc_command = b.addSystemCommand(&.{ resource_compiler, "examples/resources.rc" });
-            const output = rc_command.addPrefixedOutputFileArg("-fo", "resources.res");
-            if (!is_dynamic) rc_command.addArg("-D_UI_STATIC");
-            lib.addObjectFile(output);
+            const rc_command = b.addSystemCommand(&.{resource_compiler});
+            // options
+            rc_command.addArgs(&.{ "/:auto-includes", "gnu" });
+            if (!is_dynamic) rc_command.addArgs(&.{ "/d", "_UI_STATIC" });
+
+            // input file
+            rc_command.addArg("examples/resources.rc");
+            // output file
+            const output = rc_command.addOutputFileArg("resources.res");
+
+            exe.addObjectFile(output);
+            exe.step.dependOn(&rc_command.step);
         }
 
         b.installArtifact(exe);
