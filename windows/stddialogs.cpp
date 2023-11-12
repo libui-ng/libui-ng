4bd9f1bf06fee3521bf638ae90f97c6e125e2014
Louis Pearson <opensource@louispearson.work>
2023-11-11 19:33:51 -0700

    Apply suggestions from matyalatte
    Start filterpattern for loop from 1, convert pattern strings to UTF16
    
    Co-authored-by: matyalatte <matyalatte@gmail.com>

--- windows/stddialogs.cpp
+++ windows/stddialogs.cpp
@@ -57,12 +57,15 @@ char *commonItemDialog(HWND parent, REFCLSID clsid, REFIID iid, FILEOPENDIALOGOP
 				// assert(filter.patternCount > 0);
 
 				filterpatterns[s] = toUTF16(filter.patterns[pattern]);
-				for (pattern = 0; pattern < filter.patternCount; pattern++) {
+				for (pattern = 1; pattern < filter.patternCount; pattern++) {
 					wchar_t *old;
+					wchar_t *next;
 
 					old = filterpatterns[s];
-					filterpatterns[s] = strf(L"%s;%s", filterpatterns[s], filter.patterns[pattern]);
+					next = toUTF16(filter.patterns[pattern]);
+					filterpatterns[s] = strf(L"%ls;%ls", old, next);
 					uiprivFree(old);
+					uiprivFree(next);
 				}
 
 				filternames[s] = toUTF16(filter.name);
